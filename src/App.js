4ce530fd520571bf4ca97b8cde70e62e9cf95d7f
Christo Johnson <christo@francium.tech>
2022-07-04 13:49:50 +0530

    add task, mark as done, delete task

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [toDos,setToDos] = useState([])
+  const [toDo,setToDo] = useState('')
+
   return (
     <div className="app">
       <div className="mainHeading">
@@ -9,22 +12,55 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        <h2>Whoop, add your tasks... 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>{setToDos([...toDos,{id:Date.now(),text:toDo,status:false}]); setToDo('');}} className="fas fa-plus"></i>
       </div>
       <div className="todos">
-        <div className="todo">
+        { toDos.map((obj,index)=>{
+          return(
+          <div key={obj.id} className="todo">
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
+            <input onChange={(e)=>{
+              setToDos(toDos.filter(obj2=>{
+                if(obj2.id===obj.id){
+                  obj2.status=e.target.checked
+                  console.log(obj)
+                }
+                return obj2.id
+              }))
+            }} type="checkbox" name="" value={obj.status} id="" />
+            <p>{obj.text}</p>
+            </div>
+            <div className="right">
+              
+            {/* <i className="fas fa-times" ind={index} onClick={remove_task}></i> */}
+
+              <i className="fas fa-times" onClick={()=>{
+                let new_array = toDos.filter(delObj=>{
+                  if(delObj.id!==obj.id){
+                    console.log("object id: ",obj.id,"delete object: ",delObj.id)
+                    console.log(delObj)
+                    return true
+                  }
+                  return false
+                })
+                setToDos([...new_array])
+              }}></i>
+            </div>
           </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+        )  
+      }) }
+      {
+        toDos.map((disObj)=>{
+            if(disObj.status===true){
+              return (<h1 key={disObj.id}>{disObj.text}</h1>)
+            }
+            return null
+        })
+      }
       </div>
     </div>
   );
